9fcca8fbaebd9bbad8b57391086c67f0881044bd
bnyt_jane <bnyt_jane@163.com>
2020-03-09 17:11:26 +0800

    基本数据结构

--- Exercise/01.hello.py
+++ Exercise/01.hello.py
@@ -1,4 +1,5 @@
+# 注释
 print('Hello world')
-print("Hello world")
-print('Hello',"Python")
-print('My name is ',input('Please enter your name...'))
+print("Hello world") # 单引号、双引号
+print('Hello',"Python") # 逗号
+print('My name is ',input('Please enter your name...')) #输入
---
+++ Exercise/02.example.py
@@ -0,0 +1,7 @@
+import time
+
+print(time.time())
+
+if 10-9 > 0:
+    # 测试语句块
+    print('10 > 9')
