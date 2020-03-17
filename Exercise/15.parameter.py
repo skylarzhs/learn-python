b91986db2e2bc78e7dcf930141bd2b76ca6b809a
Jane <157519788@qq.com>
2020-03-17 22:11:13 +0800

    递归

--- Exercise/15.parameter.py
+++ Exercise/15.parameter.py
@@ -105,15 +105,145 @@
 
 # 关键字参数
 
-def person(name, age, **other):
-    print('name:', name, 'age:', age, 'other', other)
+# def person(name, age, **other):
+#     print('name:', name, 'age:', age, 'other', other)
 
 
-person('Lisa', 12)
-person('Tom', 21, city='JN')
-person('Lisa', 12, city='LW', job='Engineer')
+# person('Lisa', 12)
+# person('Tom', 21, city='JN')
+# person('Lisa', 12, city='LW', job='Engineer')
 
 
-info = {'job':'PHPer','city':'BJ','sex':'F'}
+# info = {'job':'PHPer','city':'BJ','sex':'F'}
 
-person('Jane',32,**info)
+# person('Jane',32,**info)
+
+# 命名关键字参数
+
+# def person(name, age, *, city, job):
+#     print(name, age, city, job)
+
+
+# person('A', 12, city='SD', job='Eng')
+
+# person('B', 12, job='Eng',city='JN') # 与位置无关
+
+# person('C', 12) # ERROR
+
+# Traceback (most recent call last):
+#   File ".\Exercise\15.parameter.py", line 131, in <module>
+#     person('C', 12)
+# TypeError: person() missing 2 required keyword-only arguments: 'city' and 'job'
+
+# person('B', 12, 'Eng', 'JN') #ERROR 命名关键字必须传入变量名
+# Traceback (most recent call last):
+#   File ".\Exercise\15.parameter.py", line 138, in <module>
+#     person('B', 12, 'Eng', 'JN')
+# TypeError: person() takes 2 positional arguments but 4 were given
+
+# 函数中已经有一个可变参数，后面的命名关键字参数不再需要一个特殊分隔符`*`
+
+# def person(name, age, *args, city, job='Teacher'):
+#     print(name, age, args, city, job)
+
+
+# person('D', 12, 999,'AAAAA', city='BJ')
+
+# 关键字参数兼容可变参数，不兼容关键字参数。
+# def person(name, age, **args, city, job='Teacher'):
+#     print(name, age, args, city, job)
+
+
+# person('D',sex='F', year='2020', city='BJ')
+
+# PS F:\projects\learn-python> python .\Exercise\15.parameter.py
+#   File ".\Exercise\15.parameter.py", line 153
+#     def person(name, age, **args, city, job='Teacher'):
+#                                   ^
+# SyntaxError: invalid syntax
+
+# 参数组合
+
+# def func1(a, b=0, *c, **d):
+#     print(a, b, c, d)
+
+
+# func1(1, 2, 3, 4, 5)
+
+# func1(6, 999, 'ppp',e=444, f=888)
+
+# def f1(a, b, c=0, *args, **kw):
+#     print(a, b, c, args, kw)
+
+
+# def f2(a, b, c=0, *, d, **kw):
+#     print(a, b, c, d, kw)
+
+
+# f1(1, 2)
+
+# f1(1, 2, 3)
+
+# f1(1, 2, 3, 4, 5)
+
+# f1(1, 2, 3, 4, 5, cc=9, m='p')
+
+# f2(1,2,3,d=999)
+
+# f2(2,3,4,d=898,dd='opq')
+
+# list tuple
+
+# args = (1, 2, 3, 4)
+
+# kw = {'kw1': 1122, 'kw2': 990}
+
+# f1(*args, *kw) #单个*取键名作为可变参数的一部分
+# # 1 2 3 (4, 'kw1', 'kw2') {}
+
+# f1(*args, **kw)
+# # 1 2 3 (4,) {'kw1': 1122, 'kw2': 990}
+
+
+# 练习题目
+
+# def product(x, y):
+#     return x*y
+
+def product(x, *nums):
+    if not isinstance(x, (int, float)):
+        raise TypeError('Type ERROR')
+    m = x
+    for i in nums:
+        if not isinstance(i, (int, float)):
+            raise TypeError('Type ERROR')
+        m = m * i
+    return m
+
+
+# print(product(1))
+# print(product(1, 2))
+# print(product(1, 2, 3))
+# print(product(1, 2, 3, 4))
+
+# print(product(1, 'p'))
+
+# 测试
+print('product(5) =', product(5))
+print('product(5, 6) =', product(5, 6))
+print('product(5, 6, 7) =', product(5, 6, 7))
+print('product(5, 6, 7, 9) =', product(5, 6, 7, 9))
+if product(5) != 5:
+    print('测试失败!')
+elif product(5, 6) != 30:
+    print('测试失败!')
+elif product(5, 6, 7) != 210:
+    print('测试失败!')
+elif product(5, 6, 7, 9) != 1890:
+    print('测试失败!')
+else:
+    try:
+        product()
+        print('测试失败!')
+    except TypeError:
+        print('测试成功!')
---
+++ Exercise/16.recurse.py
@@ -0,0 +1,47 @@
+# -*- encoding : utf-8 -*-
+
+# 计算某值的阶乘
+
+# 递归函数：调用自身，处理临界值
+# def fact(n):
+#     if not isinstance(n, (int,)):
+#         raise TypeError('Please enter a int number')
+#     if n < 1:
+#         raise TypeError('type error')
+#     if n > 1:
+#         m = n * fact(n-1)
+#     else:
+#         m = 1
+
+#     return m
+
+# 尾递归修改
+
+# print(fact(1))
+# print(fact(2))
+# print(fact(3))
+# print(fact(4))
+# print(fact(5))
+
+# print(fact(1000)) #栈溢出
+# RecursionError: maximum recursion depth exceeded in comparison
+
+
+# def fact(n):
+#     return fact_iter(n, 1)
+
+
+# def fact_iter(num, product):
+#     if num == 1:
+#         return product
+#     return fact_iter(num-1, num*product) # 计算结果
+
+# print(fact(1000)) #栈溢出，结果看还是有栈溢出，python解析器没有做此优化
+
+# 练习，汉诺塔的移动 
+# H(1) = 1
+# H(n) = 2H(n-1) + 1(n>1)
+
+def move(n,a,b,c):
+    
+
