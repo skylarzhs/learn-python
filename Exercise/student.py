24bdbb97f4bec58b33e23d9a15a4307f911595cd
bnyt_jane <bnyt_jane@163.com>
2020-03-24 17:18:32 +0800

    OOP类和实例

---
+++ Exercise/student.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- encoding : utf-8 -*-
+
+'A class test'
+
+__author__ = 'Skylar Zheng'
+
+
+class Student(object):
+
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
