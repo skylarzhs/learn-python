94a8550c35c0447c8b0378d5e7e263a151199868
bnyt_jane <bnyt_jane@163.com>
2020-04-28 17:24:48 +0800

    修改HTML解析

--- Exercise/74.html_exercise.py
+++ Exercise/74.html_exercise.py
@@ -5,29 +5,59 @@ from html.parser import HTMLParser
 from urllib import request
 import re
 
-L = []
-i = 0
-n = 0
-t = 0
-d = 0
-addr = 0
-
 
 class MyEventsParser(HTMLParser):
+
+    L = []
+    i = 0
+    record = 0
+    n = 0
+    t = 0
+    addr = 0
+
     def handle_starttag(self, tag, attrs):
-        pass
+        # print(attrs)
+        if tag == 'ul' and 'class' in attrs and attrs[0][1] == 'list-recent-events':
+            print('ul start')
+            self.record = 1
+        elif tag == 'li' and self.record == 1:
+            self.L[self.i] = dict()
+        elif tag == 'a' and self.record == 1:
+            self.n = 1
+        elif tag == 'time' and self.record == 1:
+            self.L[self.i]['time'] = attrs['datetime']
+        elif tag == 'span' and self.record == 1:
+            self.addr = 1
 
     def handle_endtag(self, tag):
-        pass
+        if tag == 'ul' and self.record == 1:
+            self.record = 0
+        elif tag == 'li' and self.record == 1:
+            self.i = self.i + 1
+            # 储存值
+        elif tag == 'a' and self.record == 1:
+            self.n = 0
+        elif tag == 'span' and self.record == 1:
+            self.addr = 0
 
     def handle_data(self, data):
-        print(data)
+        data = data.replace(r'\n', '')
+        if data:
+            if self.n == 1:
+                self.L[self.i]['name'] = data
+            elif self.addr == 1:
+                self.L[self.i]['addr'] = data
 
 
 parser = MyEventsParser()
 # with request.urlopen('https://www.python.org/events/python-events/') as f:
 with open(r'E:\projects\learn-python\Tutorial\static\html\events.html', 'r') as f:
-    html = f.read().replace('\n', '').replace('&nbsp;', '')
-    # print(html)
-    content = re.match(r'\<ul class="list-recent-events menu"\>', html)
-    print(content)
+    html = f.read().replace(r'\n', '')
+    # content = re.match(r'<ul class="list-recent-events menu">.+?<ul>', html)
+    # print(content)
+
+
+parser = MyEventsParser()
+parser.feed(html)
+
+print(parser.L)
