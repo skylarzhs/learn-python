c47839a5fbb1dc8cab201510a194e350800251b9
bnyt_jane <bnyt_jane@163.com>
2020-04-29 16:59:24 +0800

    third-party module Pillow

---
+++ Exercise/75.pillow.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding : utf-8 -*-
+
+from PIL import Image,ImageFilter
+
+# im = Image.open(r'E:\projects\learn-python\Tutorial\static\image\64.png')
+
+# 缩放
+# w, h = im.size
+
+# print('Original image size:%s x %s' % (w, h))
+
+# im.thumbnail((w//2, h//2))
+
+# im.save(r'E:\projects\learn-python\Tutorial\static\image\64_2.png')
+
+# 模糊
+
+# im2 = im.filter(ImageFilter.BLUR)
+
+# im.save(r'E:\projects\learn-python\Tutorial\static\image\64_blur.png')
+
+# 生成验证码
